4cc7f201bbcc40b64c462285fb98fae8bc7f5529
John Bananas <johnbananas@Johns-MacBook-Pro.local>
2022-07-16 18:01:15 -0400

    Fixed bug in setup.sh bug where a directory was accidentally hard-coded

--- setUp.sh
+++ setUp.sh
@@ -1,4 +1,5 @@
+FILES=$PWD;
 cd ~;
 for entry in `ls -A ~/Downloads/BenRazProjects/DotFiles/theFiles`; do
-    ln -s -F ~/Downloads/BenRazProjects/DotFiles/theFiles/$entry ~/$entry;
+    ln -s -F $FILES/$entry ~/$entry;
 done
